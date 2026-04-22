4fc4a32b1f0615f3bb1c1d45eb0394151d3f08da
el21np <el21np@leeds.ac.uk>
2026-04-22 13:17:57 +0100

    fix: change base

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ecomm-store',
+  base: '/ecomm-store/',
   plugins: [react()],
   server: {
     proxy: {
