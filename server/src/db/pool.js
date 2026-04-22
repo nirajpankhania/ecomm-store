8841b450053fa85da9153bda16fed3e016935f59
el21np <el21np@leeds.ac.uk>
2026-04-22 13:59:33 +0100

    fix: force ssl

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL?.includes('render.com') 
+    ssl: process.env.DATABASE_URL 
         ? { rejectUnauthorized: false } 
         : false,
 });
